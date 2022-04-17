9d6c22780c733b3fcc30e0ac6dc404ea8f1702c9
Antonio Silva <antoniopsilva083@gmail.com>
2022-04-17 02:18:31 -0300

    feat: create header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
-// import Head from 'next/head'
+import Head from 'next/head'
 // import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      Home
+    </>
   )
 }
