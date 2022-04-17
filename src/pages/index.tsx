aa263440ef2aad4a2221cf043dc68703350a5df1
Antonio Silva <antoniopsilva083@gmail.com>
2022-04-17 12:12:37 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-// import Image from 'next/image'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -7,7 +7,19 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      Home
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
