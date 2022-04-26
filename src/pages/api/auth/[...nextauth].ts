22a05912b63ff22bb9e7204ad791418272a85744
Antonio Silva <antoniopsilva083@gmail.com>
2022-04-26 01:09:40 -0300

    feat: save user to database and check duplicate user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -12,7 +16,41 @@ export default NextAuth({
           scope: 'read:user',
         },
       }
-    }),
-    // ...add more providers here
+    }),    
   ],
+  // ...add more providers here
+  callbacks: {
+    async signIn({ user, account, profile }): Promise<boolean> {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 })
